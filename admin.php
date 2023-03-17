752e1a9aeeced6764ce9dbea5b675270e3bbc5ab
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-03-17 22:17:11 +0800

    new

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+<body>
+
+    <h1>Welcome, Admin!</h1>
+
+    <!-- Add your admin dashboard content here -->
+
+    <form action="logout.php" method="post">
+        <input type="submit" value="Logout">
+    </form>
+
+</body>
+</html>
