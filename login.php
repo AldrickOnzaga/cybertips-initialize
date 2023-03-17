deace487738008817803c9b5100365bf79da00c8
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-03-17 22:17:21 +0800

    updated

--- login.php
+++ login.php
@@ -12,22 +12,43 @@
     <body>
     <?php
 
-    // Check if the form was submitted
-    /*if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        @include 'config.php';
 
-        // Get the username and password from the form data
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        session_start();
 
-        // TODO: Add validation and authentication logic here
-        // For example, you might check if the username and password
-        // match a user account in your database
+        if(isset($_POST['submit'])){
 
-        // If the username and password are valid, redirect the user
-        // to a protected page
-        header("Location: protected.php");
-        exit;
-    }*/
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $pass = md5($_POST['password']);
+        $cpass = md5($_POST['cpassword']);
+        $user_type = $_POST['user_type'];
+
+        $select = " SELECT * FROM user_list WHERE email = '$email' && password = '$pass' ";
+
+        $result = mysqli_query($conn, $select);
+
+        if(mysqli_num_rows($result) > 0){
+
+            $row = mysqli_fetch_array($result);
+
+            if($row['user_type'] == 'admin'){
+
+                $_SESSION['admin_name'] = $row['name'];
+                header('location:admin.php');
+
+            }elseif($row['user_type'] == 'user'){
+
+                $_SESSION['user_name'] = $row['name'];
+                header('location:userdash.php');
+
+            }
+            
+        }else{
+            $error[] = 'incorrect email or password!';
+        }
+
+        };
     ?>
         <nav>
             <input type="checkbox" id="check">
--- register.php
+++ register.php
@@ -1,3 +1,37 @@
+<!--connection and program-->
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['submit'])){
+
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$pass = md5($_POST['password']);
+$cpass = md5($_POST['cpassword']);
+$user_type = $_POST['user_type'];
+
+$select = " SELECT * FROM user_list WHERE email = '$email' && password = '$pass' ";
+
+$result = mysqli_query($conn, $select);
+
+if(mysqli_num_rows($result) > 0){
+
+    $error[] = 'user already exist!';
+
+}else{
+
+    if($pass != $cpass){
+        $error[] = 'password not matched!';
+    }else{
+        $insert = "INSERT INTO user_list(name, email, password, user_type) VALUES('$name','$email','$pass','$user_type')";
+        mysqli_query($conn, $insert);
+        header('location:login.php');
+    }
+}
+};
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,25 +45,7 @@
         <!--new code-->
     </head>
     <body>
-    <?php
 
-    // Check if the form was submitted
-    /*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // Get the username and password from the form data
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        // TODO: Add validation and authentication logic here
-        // For example, you might check if the username and password
-        // match a user account in your database
-
-        // If the username and password are valid, redirect the user
-        // to a protected page
-        header("Location: protected.php");
-        exit;
-    }*/
-    ?>
         <nav>
             <input type="checkbox" id="check">
             <label for="check" class="checkbtn">
