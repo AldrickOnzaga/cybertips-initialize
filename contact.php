0dad6cd6108c520c68a9a5403134f171cd9b1f9e
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-03-21 08:56:57 +0800

    updated

--- contact.php
+++ contact.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/style1.css">
         <link rel="stylesheet" href="css/nav.css">
+        <link rel="stylesheet" href="css/contact_form.css">
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     </head>
     <body>
@@ -60,18 +61,20 @@
                     }
                 }
             ?>
-            <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
-                <br>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-                <br>
-                <label for="message">Message:</label>
-                <textarea id="message" name="message" rows="5" required></textarea>
-                <br>
-                <input type="submit" value="Send">
-            </form>
+            <div class="contact_form">
+                <form method="POST">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required>
+                    <br>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                    <br>
+                    <label for="message">Message:</label>
+                    <textarea id="message" name="message" rows="5" required></textarea>
+                    <br>
+                    <input type="submit" value="Send">
+                </form>
+            </div>
         </main>
         
         <footer>
