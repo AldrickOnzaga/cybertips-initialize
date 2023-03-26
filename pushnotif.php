ee0c806bbecc3f41f74e9ca869122dd61490cae6
Aldrick Onzaga <aonzaga.k11720977@umak.edu.ph>
2023-03-26 18:03:43 +0800

    new

---
+++ pushnotif.php
@@ -0,0 +1,42 @@
+<?php
+
+    @include_once 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Dashboard</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/table.css">
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    </head>
+    <body>
+        <div class="menu">
+                <img id="logo" class="logo" src="img/cyb6.png" alt="CYBERTIPS Logo">
+                <ul>
+                    <li><a href="admin.php">Dashboard</a></li>
+                    <li><a href="CM.php">Content managment</a></li>
+                    <li><a href="pushnotif.php">Push Notification</a></li>
+                    <li><a href="index.php">CYBERTIPS</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+        </div>
+        <div class="body">
+            <h1>POST</h1>
+        </div>
+        <footer>
+                <p>CYBERTIPS</p>
+        </footer>
+        <script>
+            const menu = document.querySelector('.menu');
+            const menuBtn = document.querySelector('.menu-btn');
+            menuBtn.addEventListener('click', () => {
+                menu.classList.toggle('active');
+            });
+        </script>
+    </body>
+</html>
